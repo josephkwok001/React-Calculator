832c423ddbf7a63b4a1ae1d3a05e3addaf01508f
Joseph <177612090+josephkwok001@users.noreply.github.com>
2026-05-28 16:30:59 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Buttons from './Buttons.jsx'
+
+function App() {
+  return (
+    <div className = "calculator">
+      <div className = "output">
+        <h3> 0 </h3>
+      </div>
+      <Buttons />
+    </div>
+  )
+}
+
+export default App
